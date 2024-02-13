066d5a0985441d17a085dd9211c5a31c30fb8b38
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-02-13 11:57:31 -0500

    Implement Hero section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,17 +12,29 @@ const Hero = () => (
           <span className='text-white'>1 Month</span> Account
         </p>
       </div>
+
       <div className='flex flex-row justify-between items-center w-full'>
         <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
           The Next <br className='sm:block hidden' /> {' '}
           <span className='text-gradient'>Generation</span> {' '}
-          Payment Method.
         </h1>
+
         <div className='ss:flex hidden md:mr-4 mr-0'>
           <GetStarted />
         </div>
       </div>
+
+      <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>Payment Method</h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt='billing' className='w-[100%] h-[100%] relative z-[5]' />
+      </div>
     </div>
+
+    <div></div>
+    
+    <div></div>
   </section>
 )
 
